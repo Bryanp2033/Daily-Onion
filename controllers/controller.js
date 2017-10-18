6e41e081434495282296190b45bd66755c2195d6
Bryan <bryanporras2033@gmail.com>
2017-10-18 00:24:39 -0400

    added search route and view

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var cheerio = require("cheerio");
+var request = require("request");
+
 //Home Page
-router.get("/", function(request, response){
-    response.render("index", {layout: 'main'})
+router.get("/", function(req, res){
+    res.render("index", {layout: 'main'})
     console.log("Working Home Page");
-})
+});
+
+//Saved Page
+router.get("/saved", function(req, res){
+    res.render("saved", {layout: 'main'});
+    console.log("Working Search Page")
+});
 
 module.exports = router;
